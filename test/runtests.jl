a9aeddd39aa03cc5297b348a3bd24ffd45f81af3
Daniel O'Malley <omalled@lanl.gov>
2016-03-16 17:33:55 -0600

    progress

---
+++ test/runtests.jl
@@ -0,0 +1,43 @@
+using Base.Test
+import LinearAdjoints
+import Calculus
+
+include("testtransforms.jl")
+include("diagonaltest.jl")
+include("laplaciantest.jl")
+
+#=
+q2 = quote
+	@LinearAdjoints.assemblesparsematrix (a, c) x function f(a, b, c)
+		@assert length(a) == length(b) - 1
+		@assert length(a) == length(c)
+		I = Int[]
+		J = Int[]
+		V = Float64[]
+		for i = 1:length(a)
+			LinearAdjoints.addentry(I, J, V, i + 1, i, a[i] + b[i] ^ 2)
+		end
+		for i = 1:length(b)
+			LinearAdjoints.addentry(I, J, V, i, i, b[i])
+		end
+		for i = 1:length(c)
+			LinearAdjoints.addentry(I, J, V, i, i + 1, log(c[end - i + 1]))
+		end
+		return sparse(I, J, V)
+	end
+end
+#@show macroexpand(q2)
+#eval(q2)
+q3 = quote
+	@LinearAdjoints.assemblesparsematrix (a, c) x function f(a, b, c)
+		LinearAdjoints.addentry(I, J, V, 1, 2, a[1] ^ 3 + exp(c[end - 1 + 1, j]))
+		LinearAdjoints.addentry(I, J, V, 2, 1, a[2] ^ 3 + exp(c[end - 2 + 1, j]))
+		#LinearAdjoints.addentry(I, J, V, 1, 2, a ^ 3 + exp(c[1 - 1 + 1]))
+	end
+end
+macroexpand(q3)
+#eval(q3)
+eval(q2)
+#LinearAdjoints.adjointsparsematrix(:(LinearAdjoints.addentry(I, J, V, 1, 2, a[1])))
+=#
+
